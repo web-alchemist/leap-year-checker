85016d0e2527ade2dfbb283947fa53d6ad7f35af
web-alchemist <s-amadou.taal@lwtech.edu>
2025-11-18 01:13:12 -0800

    fix deploy branch and adapter path

--- svelte.config.js
+++ svelte.config.js
@@ -2,15 +2,9 @@ import adapter from '@sveltejs/adapter-static';
 
 export default {
 	kit: {
-		adapter: adapter({
-			pages: 'build',
-			assets: 'build',
-			fallback: undefined,
-			precompress: false,
-			strict: true
-		}),
+		adapter: adapter(),
 		paths: {
-			base: process.env.NODE_ENV === 'production' ? '/leap-year-checker' : ''
+			base: '/leap-year-checker'
 		}
 	}
 };
