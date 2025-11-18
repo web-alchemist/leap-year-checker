b6c65087a4d69717f934d7e14b5cc640f9ad0b7f
web-alchemist <s-amadou.taal@lwtech.edu>
2025-11-18 01:33:34 -0800

    wrote to docs

--- svelte.config.js
+++ svelte.config.js
@@ -2,7 +2,10 @@ import adapter from '@sveltejs/adapter-static';
 
 export default {
 	kit: {
-		adapter: adapter(),
+		adapter: adapter({
+			pages: 'docs',
+			assets: 'docs'
+		}),
 		paths: {
 			base: process.env.NODE_ENV === 'production' ? '/leap-year-checker' : ''
 		}
