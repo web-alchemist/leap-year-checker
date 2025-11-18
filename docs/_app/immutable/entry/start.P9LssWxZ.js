d657f0219bc59a9f51896ecf80737bba22bd20fc
web-alchemist <s-amadou.taal@lwtech.edu>
2025-11-18 01:58:59 -0800

    path correction

--- docs/_app/immutable/entry/start.P9LssWxZ.js
+++
@@ -1,1 +0,0 @@
-import{l as o,a as r}from"../chunks/CRA0AYYb.js";export{o as load_css,r as start};
--- svelte.config.js
+++ svelte.config.js
@@ -4,11 +4,11 @@ export default {
 	kit: {
 		adapter: adapter({
 			pages: 'docs',
-			assets: 'docs'
+			assets: 'docs',
+			fallback: 'index.html'
 		}),
 		paths: {
 			base: process.env.NODE_ENV === 'production' ? '/leap-year-checker' : ''
 		}
-	},
-	trailingSlash: 'always'
+	}
 };
