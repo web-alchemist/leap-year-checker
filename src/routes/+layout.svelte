8a762cd83400f51ec8b735c9d1a60608bde9e98b
Taal <34554599+web-alchemist@users.noreply.github.com>
2026-02-16 16:56:01 -0800

    Import Google Fonts in layout.svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,6 @@
 <script>
 	import favicon from '$lib/assets/favicon.svg';
+	@import url('https://fonts.googleapis.com/css2?family=Anonymous+Pro:wght@400;700&family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap');
 
 	let { children } = $props();
 </script>
